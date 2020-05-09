bfd3a688792cf78e8145fc3c30e7223d86fa5cb9
Shadab <shadab14meb346@gmail.com>
2020-05-09 18:04:37 +0530

    test get api

--- controllers/markdown.js
+++ controllers/markdown.js
@@ -4,35 +4,39 @@ const API_ENDPOINT = "https://api.github.com/markdown";
 const TOKEN = process.env.TOKEN;
 const axios = require("axios");
 exports.markdown = asyncHandler(async (req, res, next) => {
-	const { markdownText } = req.body;
-	axios({
-		method: "POST",
-		url: API_ENDPOINT,
-		headers: {
-			Authorization: `token ${TOKEN}`,
-			"Content-Type": "application/json",
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Headers":
-				"Origin, X-Requested-With, Content-Type, Accept",
-			"Content-Type": "application/json",
-			"Access-Control-Allow-Methods": "*",
-			"Access-Control-Max-Age": "2592000",
-			"Access-Control-Allow-Credentials": "true",
-		},
-		data: {
-			text: markdownText,
-			mode: "gfm",
-			context: "github/gollum",
-		},
-	})
-		.then((response) =>
-			res.status(200).json({
-				status: "success",
-				data: response.data,
-			}),
-		)
-		.catch((error) => ({
-			statusCode: 422,
-			body: String(error),
-		}));
+	res.status(200).json({
+		status: "success",
+		data: "Welcome",
+	});
+	// const { markdownText } = req.body;
+	// axios({
+	// 	method: "POST",
+	// 	url: API_ENDPOINT,
+	// 	headers: {
+	// 		Authorization: `token ${TOKEN}`,
+	// 		"Content-Type": "application/json",
+	// 		"Access-Control-Allow-Origin": "*",
+	// 		"Access-Control-Allow-Headers":
+	// 			"Origin, X-Requested-With, Content-Type, Accept",
+	// 		"Content-Type": "application/json",
+	// 		"Access-Control-Allow-Methods": "*",
+	// 		"Access-Control-Max-Age": "2592000",
+	// 		"Access-Control-Allow-Credentials": "true",
+	// 	},
+	// 	data: {
+	// 		text: markdownText,
+	// 		mode: "gfm",
+	// 		context: "github/gollum",
+	// 	},
+	// })
+	// 	.then((response) =>
+	// 		res.status(200).json({
+	// 			status: "success",
+	// 			data: response.data,
+	// 		}),
+	// 	)
+	// 	.catch((error) => ({
+	// 		statusCode: 422,
+	// 		body: String(error),
+	// 	}));
 });
--- routes/markdown.js
+++ routes/markdown.js
@@ -5,5 +5,5 @@ const router = express.Router();
 // @desc      create a tier1part
 // @route     POST /tier1part/create
 // @access    Public
-router.route("/").post(markdown);
+router.route("/").get(markdown);
 module.exports = router;
